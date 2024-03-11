5b1f2c8ffbf2c75f4ddcecfdf193c27b103898aa
Jason Hilder <jhilder95@gmail.com>
2024-03-11 07:19:55 +0200

    Latest updates.

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -42,17 +42,41 @@ func (l *Lexer) NextToken() token.Token {
 
     switch l.ch {
     case '=':
-        tok = newToken(token.ASSIGN, l.ch)
+        if l.peekChar() == '=' {
+            ch := l.ch
+            l.readChar()
+            tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
+        } else {
+            tok = newToken(token.ASSIGN, l.ch)
+        }
+    case '+':
+        tok = newToken(token.PLUS, l.ch)
+    case '-':
+        tok = newToken(token.MINUS, l.ch)
+    case '!':
+        if l.peekChar() == '=' {
+            ch := l.ch
+            l.readChar()
+            tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
+        } else {
+            tok = newToken(token.BANG, l.ch)
+        }
+    case '/':
+        tok = newToken(token.SLASH, l.ch)
+    case '*':
+        tok = newToken(token.ASTERISK, l.ch)
+    case '<':
+        tok = newToken(token.LT, l.ch)
+    case '>':
+        tok = newToken(token.GT, l.ch)
     case ';':
         tok = newToken(token.SEMICOLON, l.ch)
+    case ',':
+        tok = newToken(token.COMMA, l.ch)
     case '(':
         tok = newToken(token.LPAREN, l.ch)
     case ')':
         tok = newToken(token.RPAREN, l.ch)
-    case ',':
-        tok = newToken(token.COMMA, l.ch)
-    case '+':
-        tok = newToken(token.PLUS, l.ch)
     case '{':
         tok = newToken(token.LBRACE, l.ch)
     case '}':
@@ -120,5 +144,14 @@ func (l *Lexer) readNumber() string {
     }
 
     return l.input[position:l.position]
+}
+
+// similar to readChar() except it does not increment the position
+func (l *Lexer) peekChar() byte {
+    if l.readPosition >= len(l.input) {
+        return 0
+    } else {
+        return l.input[l.readPosition]
+    }
 }
 
